0aaa59de9f13c7e974be24172b0f5400f7244dd9
Dragan Jankovic <DraganJankovic@curiouscrimesgame.co.uk>
2024-01-09 00:03:18 +0100

    Added timestamps and created a Product model

---
+++ app/Models/Product.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory, HasUuids;
+
+    public $timestamps = true;
+    protected $table = 'products';
+    protected $primaryKey = 'uuid';
+    protected $fillable = [
+        'id',
+        'title',
+        'price',
+        'description',
+        'category',
+        'image',
+        'rate',
+        'count'
+    ];
+}
--- database/migrations/2024_01_08_223552_create_product_table.php
+++ database/migrations/2024_01_08_223552_create_product_table.php
@@ -21,6 +21,7 @@ return new class extends Migration
             $table->string('image')->nullable();
             $table->unsignedDecimal('rate')->nullable();
             $table->unsignedInteger('count')->nullable();
+            $table->timestamps();
         });
     }
 
